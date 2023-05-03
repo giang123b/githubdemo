cc9c448ba54bf1724c679b86291dac263e5168e6
Do Truong Giang <50605744+giang123b@users.noreply.github.com>
2023-05-04 00:38:14 +0700

    Update simple_interest.sh

--- simple_interest.sh
+++ simple_interest.sh
@@ -1,1 +1,1 @@
-HTML, or Hypertext Markup Language, was primarily designed to provide a means of creating structured scientific documents. HTML can embed scripting languages such as PHP or JavaScript to affect the behavior and content of web pages. The World Wide Web Consortium (W3C) maintains both the HTML and CSS standards.
+Enjoy your code
